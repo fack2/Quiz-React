2507c759935be37cdeb5933f680aba9a853a41bb
yaqootturman <yaqoot.turman@gmail.com>
2019-08-20 16:07:23 +0300

    start component :is done related #2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { Component } from 'react'
-import './App'
+import React, { Component } from "react"
+import Start from "./components/start/Start"
+
+import "./App.css"
 
 class App extends Component {
-	render() {
-		return (
-			<div className="App">
-				<p>hhi</p>
-			</div>
-		)
-	}
+  state = {
+    start: false
+  }
+  startGame = flag => {
+    this.setState({ start: flag })
+  }
+  render() {
+    return (
+      <div className="App">
+        {this.state.start ? (
+          <p>hello Game</p>
+        ) : (
+          <Start start={this.startGame} />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
