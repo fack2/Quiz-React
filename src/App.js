194df0f69fd555d5c9c442eaffc90a53b27703e9
Rand Inaim <rand.inaim95@gmail.com>
2019-08-20 14:42:36 +0300

    build file structre

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '..public/App'
+
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<p>hhi</p>
+			</div>
+		)
+	}
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,40 @@
+const data = [
+	{
+		question: 'A programmer is to a computer as a teacher is to?',
+		options: [ 'A desk', 'A library', 'A student', 'A principal' ],
+		answer: 'A student'
+	},
+	{
+		question:
+			'Imagine a car as a computer, and its engine as a program. Which of the following is an input for the engine program?',
+		options: [ 'The drivers seat', 'The gas pedal', 'The steering wheel', 'The windshleld wipers' ],
+		answer: 'The gas pedal'
+	},
+	{
+		question: 'A good algorithm must be:',
+		options: [ 'Detailed', 'Replaceable', 'Simple', 'Open-ended' ],
+		answer: 'Detailed'
+	},
+	{
+		question: 'If a program has gone through many iterations, what must be true?',
+		options: [
+			'The code is extremely complex',
+			'The code has changed a great deal over time',
+			'The code is written in more than one language',
+			'The code once contained many bugs'
+		],
+		answer: 'The code has changed a great deal over time'
+	},
+	{
+		question: 'For a chef, what is the most important variable in preparing a meal for a dinner party?',
+		options: [
+			'The time of the party',
+			'The distance to the grocery store',
+			'The band of your stove',
+			'The number of guests'
+		],
+		answer: 'The number of guests'
+	}
+]
+
+export default data
